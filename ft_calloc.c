6d5b7c1b1ca4f3374c34569a7193beafb23ea9ca
Sangho Baek <sanbaek@c3r11s1.42gyeongsan.kr>
2024-04-08 21:11:56 +0900

    split - fuction naming, calloc - num != 0

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t num, size_t size)
 {
 	void	*ptr;
 
-	if (num && (num * size) / num != size)
+	if (num != 0 && (num * size) / num != size)
 		return (NULL);
 	ptr = malloc(num * size);
 	if (ptr == NULL)
--- ft_split.c
+++ ft_split.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-static size_t	get_words_length(char const *s, char c)
+static size_t	get_info_length(char const *s, char c)
 {
 	size_t	i_i;
 	size_t	s_i;
@@ -35,22 +35,22 @@ static size_t	get_words_length(char const *s, char c)
 	return (i_i);
 }
 
-static size_t	*calloc_words(char const *s, char c)
+static size_t	*calloc_info(char const *s, char c)
 {
-	size_t	*in;
+	size_t	*info;
 	size_t	i_i;
 
-	i_i = get_words_length(s, c);
-	in = (size_t *)ft_calloc(i_i + 1, sizeof(size_t));
-	if (in == NULL)
+	i_i = get_info_length(s, c);
+	info = (size_t *)ft_calloc(i_i + 1, sizeof(size_t));
+	if (info == NULL)
 		return (NULL);
-	in[0] = 0;
+	info[0] = 0;
 	if (i_i > 1)
-		in[0] = i_i;
-	return (in);
+		info[0] = i_i;
+	return (info);
 }
 
-static size_t	*set_words(size_t *in, char const *s, char c)
+static size_t	*set_info(size_t *info, char const *s, char c)
 {
 	size_t	i_i;
 	size_t	s_i;
@@ -63,49 +63,49 @@ static size_t	*set_words(size_t *in, char const *s, char c)
 			s_i++;
 		if (s[s_i] != '\0')
 		{
-			in[i_i++] = s_i;
+			info[i_i++] = s_i;
 			while (s[s_i] != c && s[s_i] != '\0')
 				s_i++;
-			in[i_i++] = s_i - 1;
+			info[i_i++] = s_i - 1;
 		}
 	}
-	return (in);
+	return (info);
 }
 
-static char	**set_sp(char const *s, char **sp, size_t *in)
+static char	**set_words(char const *s, char **words, size_t *info)
 {
 	size_t	i_i;
 	size_t	w_i;
 
 	i_i = 1;
 	w_i = 0;
-	while (i_i < in[0])
+	while (i_i < info[0])
 	{
-		sp[w_i] = (char *)ft_calloc((in[i_i + 1] - in[i_i] + 1) + 1, 1);
-		if (sp[w_i] == NULL)
+		words[w_i] = (char *)ft_calloc((info[i_i + 1] - info[i_i] + 1) + 1, 1);
+		if (words[w_i] == NULL)
 		{
 			while (w_i)
-				free(sp[w_i--]);
-			free(sp);
+				free(words[w_i--]);
+			free(words);
 			return (NULL);
 		}
-		ft_memmove(sp[w_i++], s + in[i_i], in[i_i + 1] - in[i_i] + 1);
+		ft_memmove(words[w_i++], s + info[i_i], info[i_i + 1] - info[i_i] + 1);
 		i_i += 2;
 	}
-	free(in);
-	return (sp);
+	free(info);
+	return (words);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**sp;
-	size_t	*in;
+	char	**words;
+	size_t	*info;
 
-	in = set_words(calloc_words(s, c), s, c);
-	if (in == NULL)
+	info = set_info(calloc_info(s, c), s, c);
+	if (info == NULL)
 		return (NULL);
-	sp = (char **)ft_calloc((in[0] / 2 + 1), sizeof(char *));
-	if (sp == NULL)
+	words = (char **)ft_calloc((info[0] / 2 + 1), sizeof(char *));
+	if (words == NULL)
 		return (NULL);
-	return (set_sp(s, sp, in));
+	return (set_words(s, words, info));
 }
