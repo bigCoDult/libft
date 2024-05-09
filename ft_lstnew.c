1e83b9c8e8df6421ae921031af4e62d4ccfe1cc9
bigCoDult <qqqqmmss11@gmail.com>
2024-05-10 02:58:53 +0900

    feat : ft_lstnew()

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -10,84 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
-// void insert_front(t_list **phead, char *str);
-// void destroy(t_list *head);
+#include "libft.h"
 
-// t_list	*ft_lstnew(void *content)
-// {
-// 	t_list *head;
-// 	char* str;
-// 	str = "";
-// 	ft_memmove(str, content, ft_strlen(content));
-// 	head = NULL;
-
-// 	insert_front(&head, str);
-// 	destroy (head);
-// 	head = NULL;
-// 	return (head);
-// }
-
-// void insert_front(t_list **phead, char *str)
-// {
-
-// 	t_list	*new_node;
-
-// 	new_node = malloc(sizeof(t_list));
-// 	new_node->content = str;
-
-// 	new_node->next = *phead;
-// 	*phead = new_node;
-// }
-// void destroy(t_list *head)
-// {
-// 	t_list	*p;
-// 	p = head;
-
-// 	while (p != NULL)
-// 	{
-// 		t_list	*next = p->next;
-// 		free(p);
-// 		p = next;
-// 	}
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// size_t	len;
-	
-	// t_list	*new_node = {0};
-	
-	// new_node = (t_list *)ft_calloc(1, sizeof(t_list));
-	// if (new_node == NULL)
-	// 	return (NULL);
-	
-	// new_node->next = NULL;
-	// if (content == NULL)
-	// {
-	// 	new_node->content = NULL;
-	// 	return (new_node);
-	// }
-	// len = ft_strlen((const char *)content);
-	// new_node->content = (void *)ft_calloc(len + 1, sizeof(char));
-	// if (new_node->content == NULL)
-	// {
-	// 	free(new_node);
-	// 	return (NULL);
-	// }
-	// ft_memmove(new_node->content, content, len + 1);
-	// return (new_node);
-
+t_list *ft_lstnew(void *content) 
+{
+	t_list *new_node = malloc(sizeof(t_list));
+	if (new_node == NULL)
+		return NULL;
+	new_node->content = content;  
+	new_node->next = NULL;
+	return new_node;
+}
 
