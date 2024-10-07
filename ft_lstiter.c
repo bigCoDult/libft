d10e1855b2f4d05bbef635684fad37997ccbd51b
bigCoDult <qqqqmmss11@gmail.com>
2024-10-07 18:50:49 +0900

    [feat] ft_lstiter.c iterate adjust func to all contents

---
+++ ft_lstiter.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanbaek <sanbaek@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 18:49:31 by sanbaek           #+#    #+#             */
+/*   Updated: 2024/10/07 18:50:14 by sanbaek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	while (lst->next != NULL)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+	return ;
+}
