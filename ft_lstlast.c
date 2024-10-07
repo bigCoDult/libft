ac75120c324b94500f5d011d4569592f15769d50
bigCoDult <qqqqmmss11@gmail.com>
2024-10-07 18:27:23 +0900

    [feat] ft_lstlast.c, get last lst

---
+++ ft_lstlast.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanbaek <sanbaek@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 18:02:16 by sanbaek           #+#    #+#             */
+/*   Updated: 2024/10/07 18:23:12 by sanbaek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstlast(t_list *lst)
+{
+	t_list *last;
+
+	last = lst;
+	while (last->next != NULL)
+		last = last->next;
+	return (last);
+}
