81ac046ccdf87faeb3d7eb3d030fe00aed44fb05
bigCoDult <qqqqmmss11@gmail.com>
2024-10-07 19:10:29 +0900

    [add] ft_lstmap.c, at least it exists

---
+++ ft_lstmap.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanbaek <sanbaek@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 18:51:07 by sanbaek           #+#    #+#             */
+/*   Updated: 2024/10/07 19:09:50 by sanbaek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list **new_list;
+	t_list *new_node;
+
+	while (lst->next != NULL)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		if (new_node == NULL)
+		{
+			ft_lstclear(&new_node, del);
+			return (NULL);
+		}
+		ft_lstadd_back(new_list, new_node);
+	}
+	return (*new_list);
+}
