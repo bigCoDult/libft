6cc4d0e7cac43e9bbe135e325959a0f5ca08006b
bigCoDult <qqqqmmss11@gmail.com>
2024-10-07 18:39:23 +0900

    [edit] libft.h, add ft_lst func

--- libft.h
+++ libft.h
@@ -90,8 +90,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_printf(const char *str, ...);
 int		ft_printffmt_cdux(t_etc *etc, va_list ap, char col);
 int		ft_printffmt_sp(t_etc *etc, va_list ap, char col);
@@ -102,5 +100,22 @@ int		ft_printfhex_fd(int islower, long long unsigned int n, int fd);
 char	**ft_split(char const *str, char separator);
 int		ft_isspace(char c);
 int		ft_atoi(char *str);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
+void ft_lstadd_back(t_list **lst, t_list *new);
+void ft_lstdelone(t_list *lst, void (*del)(void*));
+void ft_lstclear(t_list **lst, void (*del)(void*));
+void ft_lstiter(t_list *lst, void (*f)(void *));
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+
+
+
+
+
+
+
 
 #endif /* LIBFT_H */
