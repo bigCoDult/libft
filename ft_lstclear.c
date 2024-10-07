97bf5d6203329e61f4c0156f8e526e51ee34926e
bigCoDult <qqqqmmss11@gmail.com>
2024-10-07 18:48:35 +0900

    [feat] ft_lstclear.c, destroy whole linked list

---
+++ ft_lstclear.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanbaek <sanbaek@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 18:45:42 by sanbaek           #+#    #+#             */
+/*   Updated: 2024/10/07 18:48:05 by sanbaek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	while (*lst != NULL)
+		ft_lstdelone(ft_lstlast(lst), del);
+	free(lst);
+}
