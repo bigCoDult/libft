deee246da05dc0e1a9ed2e09e83df08416cd9887
bigCoDult <qqqqmmss11@gmail.com>
2024-10-08 18:01:14 +0900

    [feat] ft_lstmap.c, adjust purpose

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: sanbaek <sanbaek@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 18:51:07 by sanbaek           #+#    #+#             */
-/*   Updated: 2024/10/08 16:28:27 by sanbaek          ###   ########.fr       */
+/*   Updated: 2024/10/08 18:00:22 by sanbaek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,23 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	**new_list;
-	t_list	*new_node;
+	t_list	*new_lst;
+	t_list	*head;
 
 	if (lst == NULL || f == NULL || del == NULL)
 		return (NULL);
-	new_list = (t_list **)ft_calloc(1, sizeof(t_list *));
-	while (lst->next != NULL)
+	new_lst = NULL;
+	head = NULL;
+	while (lst != NULL)
 	{
-		new_node = ft_lstnew(f(lst->content));
-		if (new_node == NULL)
+		new_lst = ft_lstnew(f(lst->content));
+		if (new_lst == NULL)
 		{
-			ft_lstclear(&new_node, del);
+			ft_lstclear(&head, del);
 			return (NULL);
 		}
-		ft_lstadd_back(new_list, new_node);
+		ft_lstadd_back(&head, new_lst);
+		lst = lst->next;
 	}
-	return (*new_list);
+	return (head);
 }
